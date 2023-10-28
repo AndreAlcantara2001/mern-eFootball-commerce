1da5fb3858c5c31279321d75888059593d89a152
AndreAlcantara2001 <corgicoder2001@gmail.com>
2023-10-29 02:13:01 +0630

    complete listing UI

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,12 +4,30 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore from 'swiper'
 import { Navigation } from 'swiper/modules'
 import 'swiper/css/bundle';
+import Contact from '../components/Contact';
+import efotEpic from '../assets/efootballEpic.png'
+import efotPotw from '../assets/efootballPotw.png'
+import efotBigTime from '../assets/efootballBigTime.jpg'
+import efotShowTime from '../assets/efootballshowTime.png'
+import {
+    FaBath,
+    FaBed,
+    FaChair,
+    FaMapMarkedAlt,
+    FaMapMarkerAlt,
+    FaParking,
+    FaShare,
+} from 'react-icons/fa';
+import { useSelector } from 'react-redux'
 
 export const Listing = () => {
     SwiperCore.use([Navigation])
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
+    const [copied, setCopied] = useState(false);
+    const {currentUser} = useSelector(state => state.user)
+    const [contact, setContact] = useState(false);
     const params = useParams()
     useEffect(() => {
         const fetchListing = async () => {
@@ -47,16 +65,96 @@ export const Listing = () => {
 
             {listing && !loading && !error && (
                 <>
-                <Swiper navigation>
-                    {listing.imageUrls.map((imageUrl) => (
-                        <SwiperSlide key={imageUrl}>
-                            <div className="h-[500px]" style={{background: `url(${imageUrl}) center no-repeat`, backgroundSize: 'cover'}}>
+                    <Swiper navigation>
+                        {listing.imageUrls.map((imageUrl) => (
+                            <SwiperSlide key={imageUrl}>
+                                <div className="h-[200px] sm:h-[500px]" style={{ background: `url(${imageUrl}) center no-repeat`, backgroundSize: 'contain' }}>
 
-                            </div>
-                        </SwiperSlide>
-                    )
+                                </div>
+                            </SwiperSlide>
+                        )
+                        )}
+                    </Swiper>
+                    <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+                        <FaShare
+                            className='text-slate-500'
+                            onClick={() => {
+                                navigator.clipboard.writeText(window.location.href);
+                                setCopied(true);
+                                setTimeout(() => {
+                                    setCopied(false);
+                                }, 1000);
+                            }}
+                        />
+                    </div>
+                    {copied && (
+                        <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+                            Link copied!
+                        </p>
                     )}
-                </Swiper>
+                    <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+            <p className='text-2xl font-semibold'>
+              {listing.name} - ${' '}
+              {listing.offer
+                ? listing.discountPrice.toLocaleString('en-US')
+                : listing.regularPrice.toLocaleString('en-US')}
+              {listing.type === 'rent' && ' / month'}
+            </p>
+            <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+              <FaMapMarkerAlt className='text-green-700' />
+              {listing.address}
+            </p>
+            <div className='flex gap-4'>
+              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md flex justify-center items-center font-semibold'>
+                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+              </p>
+              {listing.offer && (
+               <div className='bg-green-700 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+               <p style={{ textDecoration: 'line-through' }}>
+                   ${listing.regularPrice}
+               </p>
+               <p>
+                   ${listing.discountPrice} <span className='text-yellow-500 font-bold'>Now!</span>
+               </p>
+           </div>
+              )}
+            </div>
+            <p className='text-slate-800'>
+              <span className='font-semibold text-black'>Description - </span>
+              {listing.description}
+            </p>
+            <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <img src={efotEpic} alt='Epic' className='h-12 w-12 object-contain rounded-lg'/>
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} epics `
+                  : `${listing.bedrooms} epic `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+              <img src={efotPotw} alt='Potw' className='h-12 w-12 object-contain rounded-lg'/>
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} potws `
+                  : `${listing.bathrooms} potw `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+              <img src={efotBigTime} alt='BigTime' className='h-12 w-12 object-contain rounded-lg'/>
+                {listing.parking ? 'Parking spot' : 'No Parking'}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+              <img src={efotShowTime} alt='ShowTime' className=' h-12 w-12 object-contain rounded-lg'/>
+                {listing.furnished ? 'Furnished' : 'Unfurnished'}
+              </li>
+            </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+              )}
+              {contact && <Contact listing={listing} />}
+            </div>
                 </>
             )}
 
