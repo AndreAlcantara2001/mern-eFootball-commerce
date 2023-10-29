f9b53e511c4d1614e3cde34ae70ef2327da7bcd6
AndreAlcantara2001 <corgicoder2001@gmail.com>
2023-10-29 16:50:57 +0630

    add pre installation

--- api/index.js
+++ api/index.js
@@ -9,6 +9,10 @@ import listingRouter from './routes/listing.route.js'
 
 import cookieParser from 'cookie-parser';
 
+import path from 'path'
+
+const __dirname = path.resolve();
+
 app.use(express.json())
 
 app.use(cookieParser())
@@ -30,6 +34,12 @@ app.listen(PORT, () => {
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
