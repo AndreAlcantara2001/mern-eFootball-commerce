69b79b38367030058125260d077618193a425fa6
AndreAlcantara2001 <corgicoder2001@gmail.com>
2023-10-29 08:17:23 +0630

    add onChnage and onSubmit search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,31 +1,155 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Search() {
+
+    const navigate = useNavigate()
+
+    const [loading, setLoading] = useState(false)
+    const [listing, setListing] = useState([])
+
+    const [sidebarData, setSidebarData] = useState({
+        searchTerm: '',
+        type: 'all',
+        isbigtime: false,
+        islegend: false,
+        isepic: false,
+        isshowtime: false,
+        offer: false,
+        sort: 'created_at',
+        order: 'desc',
+
+    })
+
+    console.log(listing)
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+
+        const searchTermFromUrl = urlParams.get('searchTerm')
+        const typeFromUrl = urlParams.get('type')
+        const isbigtimeFromUrl = urlParams.get('isbigtime')
+        const islegendFromUrl = urlParams.get('islegend')
+        const isepicFromUrl = urlParams.get('isepic')
+        const isshowtimeFromUrl = urlParams.get('isshowtime')
+        const offerFromUrl = urlParams.get('offer')
+        const sortFromUrl = urlParams.get('sort')
+        const orderFromUrl = urlParams.get('order')
+
+        if (searchTermFromUrl ||
+            typeFromUrl ||
+            isbigtimeFromUrl ||
+            islegendFromUrl ||
+            isepicFromUrl ||
+            isshowtimeFromUrl ||
+            offerFromUrl ||
+            sortFromUrl ||
+            orderFromUrl
+        ) {
+            setSidebarData({
+                searchTerm: searchTermFromUrl || '',
+                type: typeFromUrl || 'all',
+                isbigtime: isbigtimeFromUrl === 'true' ? true : false,
+                islegend: islegendFromUrl === 'true' ? true : false,
+                isepic: isepicFromUrl === 'true' ? true : false,
+                isshowtime: isshowtimeFromUrl === 'true' ? true : false,
+                offer: offerFromUrl === 'true' ? true : false,
+                sort: sortFromUrl || 'created_at',
+                order: orderFromUrl || 'desc'
+
+            })
+        }
+
+        const fetchListings = async () => {
+            setLoading(true)
+            const searchQuery = urlParams.toString();
+            const res = await fetch(`/api/listing/get?${searchQuery}`)
+            const data = await res.json()
+            setListing(data)
+            setLoading(false)
+        }
+
+        fetchListings()
+
+    }, [location.search])
+
+
+    const handleChange = (e) => {
+        if (e.target.id === 'all' || e.target.id === 'trade' || e.target.id === 'sale') {
+            setSidebarData({ ...sidebarData, type: e.target.id })
+        }
+
+        if (e.target.id === 'searchTerm') {
+            setSidebarData({ ...sidebarData, searchTerm: e.target.value })
+        }
+
+        if (e.target.id === 'isbigtime'
+            || e.target.id === 'islegend'
+            || e.target.id === 'isshowtime'
+            || e.target.id === 'isepic'
+            || e.target.id === 'offer') {
+            setSidebarData({
+                ...sidebarData,
+                [e.target.id]: e.target.checked || e.target.checked === 'true' ? true : false
+            })
+        }
+
+        if (e.target.id === 'sort_order') {
+            const sort = e.target.value.split('_')[0] || 'created_at'
+
+            const order = e.target.value.split('_')[1] || 'desc'
+
+            setSidebarData({ ...sidebarData, sort, order })
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const urlParams = new URLSearchParams()
+        urlParams.set('searchTerm', sidebarData.searchTerm)
+        urlParams.set('type', sidebarData.type)
+        urlParams.set('isbigtime', sidebarData.isbigtime)
+        urlParams.set('islegend', sidebarData.islegend)
+        urlParams.set('isepic', sidebarData.isepic)
+        urlParams.set('isshowtime', sidebarData.isshowtime)
+        urlParams.set('offer', sidebarData.offer)
+        urlParams.set('sort', sidebarData.sort)
+        urlParams.set('order', sidebarData.order)
+
+        const searchQuery = urlParams.toString()
+
+        navigate(`/search?${searchQuery}`)
+
+    }
+
     return (
         <div className='flex flex-col md:flex-row'>
             <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
-                <form className="flex flex-col gap-8">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-8">
                     <div className="flex items-center gap-2">
                         <label className='whitespace-nowrap font-semibold'>Search Term:</label>
-                        <input type="text" id='searchTerm' placeholder='Search...' className='border rounded-lg p-3 w-full' />
+                        <input type="text" id='searchTerm' placeholder='Search...' className='border rounded-lg p-3 w-full' value={sidebarData.searchTerm} onChange={handleChange} />
                     </div>
 
                     <div className="flex gap-2 flex-wrap items-center">
                         <label className='font-semibold'>Type:</label>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='all' className='w-5' />
+                            <input type="checkbox" id='all' className='w-5' onChange={handleChange} checked={sidebarData.type === 'all'} />
                             <span className="">Trade & Sale</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='trade' className='w-5' />
+                            <input type="checkbox" id='trade' className='w-5' onChange={handleChange}
+                                checked={sidebarData.type === 'trade'}
+                            />
                             <span className="">Trade</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='sale' className='w-5' />
+                            <input type="checkbox" id='sale' className='w-5' onChange={handleChange} checked={sidebarData.type === 'sale'} />
                             <span className="">Sale</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='offer' className='w-5' />
+                            <input disabled={sidebarData.type === 'trade'} type="checkbox" id='offer' className='w-5' onChange={handleChange} checked={sidebarData.offer} />
                             <span className="">Offer</span>
                         </div>
                     </div>
@@ -33,30 +157,30 @@ export default function Search() {
                     <div className="flex gap-2 flex-wrap items-center">
                         <label className='font-semibold'>Card:</label>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='isepic' className='w-5' />
+                            <input type="checkbox" id='isepic' className='w-5' onChange={handleChange} checked={sidebarData.isepic} />
                             <span className="">Epic</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='isshowtime' className='w-5' />
+                            <input type="checkbox" id='isshowtime' className='w-5' onChange={handleChange} checked={sidebarData.isshowtime} />
                             <span className="">Show Time</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='islegend' className='w-5' />
+                            <input type="checkbox" id='islegend' className='w-5' onChange={handleChange} checked={sidebarData.islegend} />
                             <span className="">Legend</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='isbigtime' className='w-5' />
+                            <input type="checkbox" id='isbigtime' className='w-5' onChange={handleChange} checked={sidebarData.isbigtime} />
                             <span className="">Big Time</span>
                         </div>
                     </div>
 
                     <div className="flex items-center gap-2">
                         <label>Sort:</label>
-                        <select name="" id="sort_order" className="border rounded-lg p-3">
-                            <option value="">Price high to low</option>
-                            <option value="">Price low to high</option>
-                            <option value="">Latest</option>
-                            <option value="">Oldest</option>
+                        <select onChange={handleChange} defaultValue={'created_at_desc'} id="sort_order" className="border rounded-lg p-3">
+                            <option value="regularPrice_desc">Price high to low</option>
+                            <option value="regularPrice_asc">Price low to high</option>
+                            <option value="createdAt_desc">Latest</option>
+                            <option value="createdAt_asc">Oldest</option>
                         </select>
                     </div>
 
